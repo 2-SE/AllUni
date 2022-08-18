41971711f256fc91c867e3a1b0e5ff238b838f7b
LongefayGuinand-Mael <mael.longefay-guinand@esme.fr>
2022-08-18 17:15:37 +0200

    Completion of the Calendar   - Viewing an Appointment (with PopUp)
    To-Do-List the the future development :
      - Fix the Month-Click Problem of Appointment
      - Complete the EditPage with other informations :
        * description
        * color choice
        * add a deadline
        * etc.
    
     Then, pass to the others parts of the dev (FreeRoomsPage/ProfileView/etc.)

--- lib/Calendars/EventViewingPopUp.dart
+++ lib/Calendars/EventViewingPopUp.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:navigation_drawer_test/Calendars/EventEditingPage.dart';
 import 'package:navigation_drawer_test/Models/EventModel.dart';
-import 'package:navigation_drawer_test/Providers/EventProvider.dart';
-import 'package:provider/provider.dart';
+import 'package:navigation_drawer_test/Utils/DateHourUtils.dart';
 
 class EventViewingPopUp extends StatelessWidget {
   final Event event;
@@ -14,45 +12,181 @@ class EventViewingPopUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("VIEWING"),
-        leading: const CloseButton(),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(32),
-        children: [
-          Text(
-            event.title,
-            style: const TextStyle(fontWeight: FontWeight.bold),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Hero(
+          tag: event.title,
+          child: Material(
+            color: Colors.white,
+            elevation: 2,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                    MediaQuery.of(context).size.width / 24)),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            height: 20,
+                            width: 50,
+                            color: Colors.green,
+                            child: const Center(
+                              child: Text(
+                                "Cours",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        //Icon(Icons.settings)
+                        IconButton(
+                            icon: const Icon(Icons.settings), onPressed: () {}),
+                      ],
+                    ),
+                    Text(
+                      event.title,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28,
+                      ),
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 0.5,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Date :   ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          DateHourUtils.toDate(event.fromDate),
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Horaires :   ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          "${DateHourUtils.toTime(event.toDate)} - ${DateHourUtils.toTime(event.toDate)}",
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Salle :   ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          DateHourUtils.toDate(event.fromDate),
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Professeur :   ",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          DateHourUtils.toDate(event.fromDate),
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    ClipRRect(
+                      //borderRadius: BorderRadius.circular(5),
+                      //child: Text(event.title ?? "Aucune note enregistrée pour ce cours"),
+                      child: Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: Colors.grey),
+                          ),
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Text(
+                              (event.description != "")
+                                  ? event.description
+                                  : "Aucune note enregistrée pour ce cours.",
+                              maxLines: 5,
+                              overflow: TextOverflow.ellipsis,
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 16,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-          Text(
-            event.description,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            "${event.fromDate}",
-          ),
-          Text(
-            "${event.toDate}",
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => EventEditingPage(event: event)));
-            },
-            child: const Text("Editer"),
-          ),
-          TextButton(
-            onPressed: () {
-              final provider =
-                  Provider.of<EventProvider>(context, listen: false);
-              provider.deleteEvent(event);
-              Navigator.of(context).pop();
-            },
-            child: const Text("Supprimer"),
-          ),
-        ],
+        ),
       ),
     );
   }
--- lib/Providers/EventProvider.dart
+++ lib/Providers/EventProvider.dart
@@ -6,13 +6,6 @@ class EventProvider extends ChangeNotifier {
 
   List<Event> get events => _events;
 
-  //DateTime _selectedDate = DateTime.now();
-
-  //DateTime get selectedDate => _selectedDate;
-
-  //void setDate(DateTime date) => _selectedDate = date;
-  //List<Event> get eventsOfSelectedDate => _events;
-
   void addEvent(Event event) {
     _events.add(event);
     notifyListeners();
